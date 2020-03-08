d422a2ded3d0d1ff69999110d2c52072922f5c8f
Kamrul Islam Tushar <ki.tushar21@gmail.com>
2020-03-09 01:16:33 +0600

    Members page design and un-needed files removal

--- src/app/_helpers/auth-guard.ts
+++ src/app/_helpers/auth-guard.ts
@@ -13,6 +13,7 @@ export class AuthGuard implements CanActivate {
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const currentUser = this.authenticationService.currentUserValue;
         if (currentUser) {
+            console.log(route)
             // Middleware check for roles
             if (route.data.roles && route.data.roles.indexOf(currentUser.role) === -1) {
                 // If user is unauthorized then redirect to homepage
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService } from '../_services'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  isLoading = true;
+  constructor(
+    private memberService: MemberService
+  ) { }
 
   ngOnInit(): void {
+    this.memberService.getMembers()
   }
 
 }
